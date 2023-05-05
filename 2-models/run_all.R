06ae7f1c9c5158f43e37311bdde39f970729ca90
Ewan Carr <ewan.carr@kcl.ac.uk>
2023-05-05 09:52:19 +0100

    Tidy up

--- 2-models/run_all.R
+++ 2-models/run_all.R
@@ -1,9 +1,3 @@
 library(here)
-source(here("models", "binary_logistic.R"), echo = TRUE)
-source(here("models", "linear.R"), echo = TRUE)
-source(here("models", "process_posterior.R"), echo = TRUE)
-source(here("models", "adjusted_prediction_plots.R"), echo = TRUE)
-
-
-
-
+source(here("2-models", "binary_logistic.R"), echo = TRUE)
+source(here("2-models", "linear.R"), echo = TRUE)
